18e73de1b29e9ccc62b2bc26cfd72aa2ccbffac4
Tee <139244119+teeokh@users.noreply.github.com>
2024-03-26 16:25:54 +1300

    Configure SQLite database with SQLAlchemy for application. Create User and Post classes, with successful testing of addition and deletion of User and Post classes

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,34 @@
+from datetime import datetime, timezone
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__) # name = special variable that is the name of the module. Instantiates the module
+import os
 
+app = Flask(__name__) # name = special variable that is the name of the module. Instantiates the module
 app.config['SECRET_KEY'] = 'cb411410f7328c85eb01ca752b487c36'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' # Configuring the flask app to use SQLite as the SLQ for the database and specifies the database file (site.db) /// gives the path relative to the directory we're currently in
+db = SQLAlchemy(app) # Actually initialising an instance of the SQLAlchemy class
+
+class User(db.Model): # We create the User class
+    id = db.Column(db.Integer, primary_key=True) # primary_key=True means this will be unique to our user
+    username = db.Column(db.String(20), unique=True, nullable=False) # nullable = cannot be null, there must be an input. Max characters = 20
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg') # Max image hash = 20 characters. Default will be if user doesn't choose a profile pic
+    password = db.Column(db.String(60), nullable=False) 
+    posts = db.relationship('Post', backref='author', lazy=True) 
+
+    def __repr__(self): # Allows us to control what is printed when an object of the class is printed
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model): # We create the Post class
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # This is the ID of the user that authored the post
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [ # List of dictionaries for post content
     {
